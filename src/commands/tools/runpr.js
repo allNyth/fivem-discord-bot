906024d0589a1b614d85cacf0d851d3f8c45b190
allNyth <filipematheus2023@gmail.com>
2023-04-29 19:05:32 -0300

    Modal Listeners added

--- src/commands/tools/runpr.js
+++ src/commands/tools/runpr.js
@@ -1,18 +1,15 @@
 const { SlashCommandBuilder, PermissionFlagsBits, ButtonBuilder, ButtonStyle, ActionRowBuilder, Component } = require('discord.js');
 require('dotenv').config()
-const { token } = process.env
 const { mainEmbed } = require('../../models/embeds');
 const { mainBtns } = require('../../models/buttons')
-function wait(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms))
-}
+
 module.exports = {
     data: new SlashCommandBuilder().setName('runpr')
         .setDescription('Inicia o bot PROJETO RP')
         .setDefaultMemberPermissions(PermissionFlagsBits.Administrator),
     async execute(interaction) {
-        const reply = await interaction.deferReply({ephemeral: true})
-     await reply.edit({components: [mainBtns], embeds:[mainEmbed]})
+        const reply = await interaction.deferReply({ ephemeral: true })
+        await reply.edit({ components: [mainBtns], embeds: [mainEmbed] })
     },
 };
 
